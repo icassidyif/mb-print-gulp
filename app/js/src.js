e596f2b68b2d4e48e9969860f56acc18bba2d73c
Cassidy <icassidyif@gmail.com>
2019-03-29 16:25:42 +0200

    29032019_01

--- app/js/src.js
+++ app/js/src.js
@@ -1,5 +1,13 @@
 var $ = require("jquery");
+var search = $('.search');
+var btnSlide = $('.btn-slide');
 
-$('a').on('click', function(e){
-  console.log(e, this);
+btnSlide.on('click', function(){
+  search.addClass('is-active');
+});
+$(window).click(function(){
+  search.removeClass('is-active');
+});
+search.click(function(event){
+  event.stopPropagation();
 });
